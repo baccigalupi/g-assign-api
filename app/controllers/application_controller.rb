3a31029d141a411374598e40db619bae0e897e22
Kane Baccigalupi <baccigalupi@gmail.com>
2014-11-05 22:05:42 -0800

    turns off auth token since it is just an api endpoint, not very well secured

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception # turn this on if we start doing real web stuff
 
   before_filter :authenticate_with_basic
 
